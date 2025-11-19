6968a932241d91068ef6adbc5de6d0f1f5ca1d75
ishzm <zaeem0119@gmail.com>
2025-11-19 14:09:59 -0500

    added more user data to form created table added boot strap

--- server.js
+++ server.js
@@ -1,19 +1,23 @@
 const express = require('express');
-const app = express();
 
 const userRouter = require('./routes/users');
-const postRouter = require('./routes/posts');
+//const postRouter = require('./routes/posts');
 
+const app = express();
 app.set('view engine', 'ejs');
-
+app.use(express.static('public'));
+app.use(express.urlencoded({extended:true}));
+//app.use(logger);
 app.use('/users', userRouter);
-app.use('/posts', postRouter); 
+//app.use('/posts', postRouter);
+
 
 app.get('/', (req, res) => {
-  console.log('Ish is cool!');
-  res.render('index', { user: 'Ish' });
+    console.log('Ish is cool!');
+    res.render("index", {user: "Ish"});
 });
 
-app.listen(3030, () => {
-  console.log('Server running on port 3030');
+app.get(`/new`, (req, res) => {
+    res.send("users/new", {firstName: "Please Enter Your Name"});
 });
+app.listen(3030);
--- users.js
+++ users.js
@@ -2,9 +2,58 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.send('User List');
+    res.render(`users/list`, { users: users });
 });
-router.get('/new', (req, res) => {
-  res.send('New User Form');
+router.get(`/new`, (req, res) => {
+    res.send("users/new", {firstName: "Please Enter Your Name"});
 });
+
+router.post('/', (req, res) => {
+    // res.send("User Created");
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastname;
+    const age = req.body.age;
+    const gender = req.body.gender;
+
+    const isVallid = firstName != "" && lastName!=="";//Check if they are there, added
+    if (isVallid) 
+  {
+        console.log(`Adding User: ${Firstname}`);
+        users.push({ 
+            first name: firstName,
+            lastName:lastName,
+            gender:gender,
+          age:age, //trailing comma
+          });
+        console.log(`New Set of Users: ${users}`);
+        res.send("User Created!");
+    } else {
+        console.log("Invalid User Data");
+        res.render("users/new", { firstName: firstName });
+    }
+});
+
+
+
+//router.get('/:id', (req, res) => {res.send(`Getting User Data: ${req.params.id}`);});
+router.route('/:id').get((req, res) => {
+    res.send(`Getting User Data: ${req.params.id}`);
+}).delete((req, res) => {
+
+}).put((req, res) => {
+    res.send(`Updating User Data: ${req.params.id}`);
+});
+
+router.param("id", (req, res, next, id) => {
+    console.log(`Accessing user # ${id}`);
+    next();
+});
+
+const users = [{ name: "Ish", role: "admin" }, { name: "Noor", role: "user" }, { name: "Amelia", role: "user" }];
+
+router.param("id", (req, res, next, id) => {
+    console.log(`Accessing user # ${id}`);
+    next();
+});
+
 module.exports = router;
